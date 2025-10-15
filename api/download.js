dd4e16428e665eba4005fd97c190dbffda66a8b8
Kaamibroken <kaamibroken64@gmail.com>
2025-10-15 11:32:32 +0500

    Add files via upload

---
+++ api/download.js
@@ -0,0 +1,9 @@
+import fetch from "node-fetch";
+
+export default async function handler(req, res) {
+  const { url } = req.query;
+  const response = await fetch(`https://api.princetechn.com/api/download/ytdlv2?apikey=prince&url=${encodeURIComponent(url)}`);
+  const data = await response.json();
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.json(data);
+}
