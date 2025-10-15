c025734d58b048e55bf2f933c16ef0e269589c88
Kaamibroken <kaamibroken64@gmail.com>
2025-10-15 11:31:50 +0500

    Add files via upload

---
+++ search.js
@@ -0,0 +1,9 @@
+import fetch from "node-fetch";
+
+export default async function handler(req, res) {
+  const { query } = req.query;
+  const response = await fetch(`https://api.princetechn.com/api/search/yts?apikey=prince&query=${encodeURIComponent(query)}`);
+  const data = await response.json();
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.json(data);
+}
